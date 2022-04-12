abccf557ce6e9a4a9ca56bba98e04bce9a7e2700
Calem Roelofs <davidnullsworth@protonmail.com>
2022-04-12 12:19:17 +0100

    Actually solved infinite loop this time

--- src/components/storylist.js
+++ src/components/storylist.js
@@ -1,12 +1,13 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import StoryCard from "./storycard";
 import { getTopStories } from "../utils/request-utils";
 
 const StoryList = (props) => {
   const [storyCards, setStoryCards] = useState([]);
+  const listHead = (((props.pageNumber - 1) * 30) + 1).toString();
 
-  (async () => {
-    const stories = await getTopStories(props.pageNumber);
+  const getStories = async (pageNumber) => {
+    const stories = await getTopStories(pageNumber);
     const storyList = stories.map((story, index) =>
       <StoryCard
         key={((props.pageNumber - 1) * 30) + index + 1}
@@ -15,7 +16,15 @@ const StoryList = (props) => {
       />
     );
     setStoryCards(storyList);
-  })();
+  };
+
+  useEffect(() => {
+    if (storyCards.length === 0 || storyCards[0].key !== listHead) {
+      getStories(props.pageNumber);
+    }
+  })
+
+
 
   return (
     <ul className="p-1 divide-y-0 self-center bg-neutral-100">
