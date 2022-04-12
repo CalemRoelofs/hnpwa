10ca0c050e3dedcf65442ce1d13349bcbef8bd78
Calem Roelofs <davidnullsworth@protonmail.com>
2022-04-12 11:27:26 +0100

    Added fly.io config Fixed infinite loop in storylist Added service workers

--- src/components/storylist.js
+++ src/components/storylist.js
@@ -1,27 +1,21 @@
-import React, { useEffect, useState } from "react";
+import React, { useState } from "react";
 import StoryCard from "./storycard";
 import { getTopStories } from "../utils/request-utils";
 
 const StoryList = (props) => {
   const [storyCards, setStoryCards] = useState([]);
-  const [pageNumber, setPageNumber] = useState(1);
 
-  useEffect(() => {
-    getStories();
-  })
-
-  const getStories = async () => {
-    setPageNumber(props.pageNumber)
-    const stories = await getTopStories(pageNumber);
+  (async () => {
+    const stories = await getTopStories(props.pageNumber);
     const storyList = stories.map((story, index) =>
       <StoryCard
-        key={((pageNumber - 1) * 30) + index + 1}
-        index={((pageNumber - 1) * 30) + index + 1}
+        key={((props.pageNumber - 1) * 30) + index + 1}
+        index={((props.pageNumber - 1) * 30) + index + 1}
         story={story}
       />
     );
     setStoryCards(storyList);
-  }
+  })();
 
   return (
     <ul className="p-1 divide-y-0 self-center bg-neutral-100">
